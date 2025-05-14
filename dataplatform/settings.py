1e02d169ec6ca8d2f89f6c275f3dbd768ff98601
Priya Sharma <ps9778228@gmail.com>
2025-05-14 17:25:04 +0530

    Page UI

--- dataplatform/settings.py
+++ dataplatform/settings.py
@@ -122,6 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 FILE_UPLOAD_HANDLERS = [
     'django.core.files.uploadhandler.TemporaryFileUploadHandler',
 ]
